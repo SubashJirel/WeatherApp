e2c177c460fe5de34b4b9902a3481161f9a55099
Subash Jirel <subashjrl@gmail.com>
2023-09-24 22:25:12 +0545

    added temperature,city,humidity, wind css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ console.log('Hello from index.js');
 import domManipulation from './dom';
 import './styles.css';
 import weatherImage from './asset/weatherImage.jpg';
-
+import rain from './asset/rain.png';
+import humidity from './asset/humidity.png';
+import wind from './asset/wind.png';
 // const imageBox = document.querySelector('#backgroundImage');
 // imageBox.src = weatherImage;
 import getWeatherData from './weather';
