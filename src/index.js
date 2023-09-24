2c7c64bbc5c2172c95ca4a1de649dc2fc5052d22
Subash Jirel <subashjrl@gmail.com>
2023-09-24 21:42:27 +0545

    added some css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('Hello from index.js');
 import domManipulation from './dom';
 import './styles.css';
 import weatherImage from './asset/weatherImage.jpg';
+
 // const imageBox = document.querySelector('#backgroundImage');
 // imageBox.src = weatherImage;
 import getWeatherData from './weather';
