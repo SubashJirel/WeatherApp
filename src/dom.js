c5e3c4ef03a7b807b4e81c3cd60bada15cba822d
Subash Jirel <subashjrl@gmail.com>
2023-09-25 22:58:20 +0545

    now tempearatures,wind,humidity of different city is rendered by searching the city name

--- src/dom.js
+++ src/dom.js
@@ -3,13 +3,29 @@ import getWeatherData from './weather';
 const dom = (() => {
   const searchBox = document.querySelector('#search-box');
   const searchBtn = document.querySelector('#search-btn');
+  const temperatureDisplay = document.querySelector('#temperature');
+  const cityBox = document.querySelector('#city');
+  const humidityName = document.querySelector('.humidity');
+  const windName = document.querySelector('.wind');
 
   searchBtn.addEventListener('click', getTemperatureOfTheCity);
 
   async function getTemperatureOfTheCity() {
     const city = searchBox.value;
     const data = await getWeatherData(city);
-    console.log('from dom.js   ', data);
+    const cityName = data.location.name;
+    const tempCelsius = data.current.temp_c;
+    const humidity = data.current.humidity;
+    const windSpeed = data.current.wind_kph;
+    render(cityName, tempCelsius, humidity, windSpeed);
+    // console.log('from dom.js   ', data);
+  }
+  function render(cityName, tempCelsius, humidity, windSpeed) {
+    console.log(cityName, tempCelsius, humidity, windSpeed);
+    temperatureDisplay.textContent = tempCelsius;
+    cityBox.textContent = cityName;
+    humidityName.textContent = `${humidity} %`;
+    windName.textContent = `${windSpeed} km/h`;
   }
 })();
 export default dom;
